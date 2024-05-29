01ac39f76caf4fcd60d59b16704c2b415f277403
yellowgene <473855713@qq.com>
2024-05-29 11:10:53 +0800

    完成了所使用数据的初始化
    完成了所使用数据的初始化

--- materialAlloc/src/hooks/decorateMap.js
+++ materialAlloc/src/hooks/decorateMap.js
@@ -1,4 +1,18 @@
 // 显示存有物资的点位
+const getMarkers = (AMap, map, availablePoint) => {
+    availablePoint.forEach((point) => {
+        // 创建 Marker 实例
+        var marker = new AMap.Marker({
+            position: new AMap.LngLat(point.longitude, point.latitude),
+            title: point.name,
+            icon: "https://a.amap.com/jsapi_demos/static/demo-center/icons/dir-via-marker.png",
+            offset: new AMap.Pixel(-13, -30),
+        });
+
+        // 将 Marker 实例添加至地图
+        map.add(marker);
+    });
+}
 // 绘制路径折线
 const getLine = (AMap, map) => {
     // 配置折线路径
@@ -14,7 +28,7 @@ const getLine = (AMap, map) => {
         strokeWeight: 2, //线条宽度
         strokeColor: "blue", //线条颜色
         lineJoin: "round", //折线拐点连接处样式
-      });
+    });
 
     // 将折线添加至地图实例
     map.add(polyline);
--- materialAlloc/src/stores/counter.js
+++ materialAlloc/src/stores/counter.js
@@ -1,12 +1,225 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const usePointStore = defineStore('point', () => {
+  const availablePoint = ref(['哈尔滨', '长春', '沈阳', '乌鲁木齐', '呼和浩特', '北京', '银川', '太原', '济南',
+    '西安', '郑州', '成都', '重庆', '武汉', '南京', '上海', '杭州', '长沙', '昆明', '福州']);  // 可用物资点位数组（名称）
+
+  // 所有物资点位详细信息数组（点位、资源数、是否可用）
+  const allPointPosition = ref([
+    {
+      name: '哈尔滨',
+      position: [126.6425, 45.757],
+      // 该点位的物资资源详情
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      // 该点位是否可用
+      available: true,
+    },
+    {
+      name: '长春',
+      position: [125.3245, 43.8868],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '沈阳',
+      position: [123.4647, 41.6775],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '乌鲁木齐',
+      position: [87.564988, 43.84038],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '呼和浩特',
+      position: [111.75199, 40.84149],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '北京',
+      position: [116.4074, 39.9042],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '银川',
+      position: [106.231, 38.4878],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '太原',
+      position: [112.5497, 37.8705],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '济南',
+      position: [117.1201, 36.6527],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '西安',
+      position: [108.9396, 34.3432],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '郑州',
+      position: [113.6581, 34.7458],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '成都',
+      position: [104.0663, 30.573],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '重庆',
+      position: [106.5505, 29.5637],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '武汉',
+      position: [114.3046, 30.5934],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '南京',
+      position: [118.7967, 32.0593],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '上海',
+      position: [121.4737, 31.2305],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '杭州',
+      position: [120.1536, 30.2875],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '长沙',
+      position: [112.9823, 28.1941],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '昆明',
+      position: [102.7123, 25.0406],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+    {
+      name: '福州',
+      position: [119.3062, 26.0753],
+      resourceDetail: {
+        'resourceA': 0,
+        'resourceB': 0,
+        'resourceC': 0,
+      },
+      available: true,
+    },
+  ])
+
+  // 修改可用物资点位（添加、删除）
+  const changeAvailablePoint = (operation, point) => {
+    // 判断操作是添加还是删除
+    if (operation === 'add') {
+      availablePoint.value.push(point)
+    } else if (operation === 'delete') {
+      availablePoint.value.filter((item) => item !== point)
+    }
   }
 
-  return { count, doubleCount, increment }
+  return { availablePoint, allPointPosition, changeAvailablePoint }
 })
